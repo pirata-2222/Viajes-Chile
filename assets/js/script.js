f37e01d64950dad2d2d1befda1a768f510754336
Piero Morata <piero.morata@gmail.com>
2021-11-13 19:23:58 -0300

    Primera versión definitiva

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+
+
+
+$(function () {
+
+    // Smooth Scrolling
+    $("a").on('click', function (event) {
+
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    // Bootstrap JS Tooltips
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    
+    // Bootstrap JS Popovers
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+});
+
